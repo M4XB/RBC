3ddb6296553534b54ec25b15577be5a5b09f5d19
M4XB <maxbanfelder@gmail.com>
2020-01-16 13:08:59 +0100

    MapTyp auf Enum geändert

--- src/RoMax_Roamer.java
+++ src/RoMax_Roamer.java
@@ -1,33 +1,58 @@
 import java.awt.* ;
 
 /** A rover that looks before it moves. */
+
+
 public class RoMax_Roamer extends Creature {
 	
 	//Variables
-		//Wall = 1
-		//Explored = 0
-	private int[][] map; 
+	//Unexplored = 0
+	//explored = 1
+	//Wall = 2
+	//
+	public enum obstacles{
+		unexplored, explored, wall
+	}
+	
+	private obstacles[][] map; 
 	
     @Override
 	public void run() {
-    	
-    	Dimension mapSize = getMapDimensions();
-    	map = new int[mapSize.height][mapSize.width];
+    	 Dimension mapSize = getMapDimensions();
+    	map = new obstacles[mapSize.height][mapSize.width];
     	printOutMap(mapSize.height, mapSize.width);
     	
         while (true) {
             
             Observation obs = observe()[0];
+            
             System.out.println(obs);
             
+            
+            //Schreibt in Map den Typ des Objektes auf das geschaut wird
+            map[obs.position.y][obs.position.x] = getObstacleAsEnum(obs.classId);
+            
+            printOutMap(mapSize.height, mapSize.width);
             int d = distance(obs.position) - 1;
             // Move until the far edge
             for (int i = 0; i < d; ++i) {
+            	//SChaut nach Links und Rechts und füllt Map
+            	turnLeft();
+            	obs = observe()[0];
+            	map[obs.position.y][obs.position.x] = getObstacleAsEnum(obs.classId);
+            	turnRight();
+            	turnRight();
+            	obs = observe()[0];
+            	map[obs.position.y][obs.position.x] = getObstacleAsEnum(obs.classId);
+            	turnLeft();
                 if (! moveForward()) {
                     // Hit something unexpected!
                     attack();
                     break;
                 }
+                //Schreibt die abgelaufenen Stellen in die Map
+                Observation self = observeSelf();
+                map[self.position.y][self.position.x] = obstacles.explored;
             }
             
             if (isEnemy(obs)) {
@@ -45,11 +70,20 @@ public class RoMax_Roamer extends Creature {
     private void printOutMap(int height, int width) {
     	for (int i=0; i<height; i++) {
     		for (int y=0; y<width; y++) {
-    			System.out.print(map[i][y]);
-    			System.out.print(" ");
+    			System.out.print(map[i][y] + " ");
     		}
     		System.out.println();
     	}
+    }
+    
+    private obstacles getObstacleAsEnum(int objectId) {
+    	obstacles obstacleId = null;
+    	switch(objectId) {
+    		case 2:
+    			obstacleId = obstacles.wall;
+    		
+    	}
+    	return obstacleId;
     }
     
     @Override
