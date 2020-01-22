a6fd32c3ef81bac730e4717b0d61807cf040bd4e
M4XB <maxbanfelder@gmail.com>
2020-01-22 11:21:21 +0100

    neu

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # RBC
 Rescue Bot Challenge
+	Wenn Kleine Map: (15 * 15)
+		nächste Wand suchen und an dieser bleiben
+		Wenn im Kreis gelaufen
+			Anders rum laufen und Lücke suchen
+	
--- src/RoMax_Roamer.java
+++ src/RoMax_Roamer.java
@@ -5,46 +5,60 @@ import java.awt.* ;
 
 public class RoMax_Roamer extends Creature {
 	
-	//Variables
-	//Unexplored = 0
-	//explored = 1
-	//Wall = 2
-	//
-	public enum obstacles{
-		unexplored, explored, wall
-	}
-	
-	private obstacles[][] map; 
+	private int[][] map; 
 	
     @Override
 	public void run() {
     	 Dimension mapSize = getMapDimensions();
-    	map = new obstacles[mapSize.height][mapSize.width];
+    	 int wallCount = 0;
+    	 boolean moved = false;
+    	map = new int[mapSize.height][mapSize.width];
     	printOutMap(mapSize.height, mapSize.width);
     	
         while (true) {
             
             Observation obs = observe()[0];
-            
             System.out.println(obs);
             
             
             //Schreibt in Map den Typ des Objektes auf das geschaut wird
-            map[obs.position.y][obs.position.x] = getObstacleAsEnum(obs.classId);
+            map[obs.position.y][obs.position.x] = obs.classId;
             
             printOutMap(mapSize.height, mapSize.width);
             int d = distance(obs.position) - 1;
+            if (d == 0) turnRight();
             // Move until the far edge
             for (int i = 0; i < d; ++i) {
-            	//SChaut nach Links und Rechts und füllt Map
-            	turnLeft();
-            	obs = observe()[0];
-            	map[obs.position.y][obs.position.x] = getObstacleAsEnum(obs.classId);
-            	turnRight();
-            	turnRight();
-            	obs = observe()[0];
-            	map[obs.position.y][obs.position.x] = getObstacleAsEnum(obs.classId);
-            	turnLeft();
+            	int obsId = obs.classId;
+            	if (!isEnemy(obs)) {
+            	
+            		moved = true;
+            	    wallCount = 0;
+            	    if(distance(obs.position) > 1){
+            		moveForward();
+            	    }else{
+            		turnRight();
+            	    }
+            	
+            	}else {
+            		switch (obsId) {
+            		case WALL_CLASS_ID:
+            			++wallCount;
+            			moved = true;
+            			if(distance(obs.position) > 1){
+            				moveForward();
+            			}else {
+            			    turnRight();
+            			}		
+            			break;
+            			
+            		case TREASURE_CLASS_ID:	
+            			attack();
+            			break;
+            		}
+            		if(!moved) enemySight(obs);
+            	}
+            	
                 if (! moveForward()) {
                     // Hit something unexpected!
                     attack();
@@ -52,16 +66,13 @@ public class RoMax_Roamer extends Creature {
                 }
                 //Schreibt die abgelaufenen Stellen in die Map
                 Observation self = observeSelf();
-                map[self.position.y][self.position.x] = obstacles.explored;
+                
             }
             
             if (isEnemy(obs)) {
                 // Attack whatever we observed
                 attack();
             }
-            
-            // Turn
-            turnRight();
         }
     }
     
@@ -76,16 +87,19 @@ public class RoMax_Roamer extends Creature {
     	}
     }
     
-    private obstacles getObstacleAsEnum(int objectId) {
-    	obstacles obstacleId = null;
-    	switch(objectId) {
-    		case 2:
-    			obstacleId = obstacles.wall;
-    		
-    	}
-    	return obstacleId;
+    
+    private void enemySight(Observation enemy){	
+    	int enemyDistance = distance(enemy.position);
+		if(enemyDistance == 2){
+	            delay();
+	        }else if(enemyDistance == 1){
+	            attack();
+	        }else{
+		    moveForward();
+		}
     }
     
+        
     @Override
 	public String getAuthorName() {
         return "Darwin SDK";
