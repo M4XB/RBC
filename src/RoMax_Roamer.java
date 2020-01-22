430883082c4bdfc997dcf352cb3f9ca243bfa923
M4XB <maxbanfelder@gmail.com>
2020-01-22 22:08:08 +0100

    simple Logic

--- src/RoMax_Roamer.java
+++ src/RoMax_Roamer.java
@@ -10,21 +10,60 @@ public class RoMax_Roamer extends Creature {
     @Override
 	public void run() {
     	 Dimension mapSize = getMapDimensions();
-    	 int wallCount = 0;
-    	 boolean moved = false;
     	map = new int[mapSize.height][mapSize.width];
-    	printOutMap(mapSize.height, mapSize.width);
+    	//printOutMap(mapSize.height, mapSize.width);
     	
-        while (true) {
-            
+    	if (mapSize.height <15 && mapSize.width < 15) {
+    		easyLogic();
+    	}else {
+    		complexAlgorithm();
+    	}   
+    }
+    
+    private void easyLogic() {
+    	Observation obs = observe()[0];
+    	if (obs.classId == WALL_CLASS_ID && distance(obs.position) == 1) {
+    		turnLeft();	
+    	}else {
+    		moveForward(1);
+    	}
+    	while(true) {
+    		obs = observe()[0];
+    		if (obs.classId == TREASURE_CLASS_ID) {
+    			attack();
+    		}
+    		if (moveForward()) {
+    			turnLeft();
+    			obs = observe()[0];
+    			if (obs.classId == WALL_CLASS_ID && distance(obs.position) == 1) {
+    				turnRight();
+        			obs = observe()[0];
+        			if (obs.classId == WALL_CLASS_ID && distance(obs.position) == 1) {
+        				turnRight();
+        			}
+    			else if (obs.classId == WALL_CLASS_ID && distance(obs.position) > 1) {
+    				moveForward(1);
+        		}
+    			}else {
+    				turnLeft();
+    			}
+    		}else {
+    			turnRight();
+    		}
+    		
+    	}
+    }
+    
+    private void complexAlgorithm() {
+    	int wallCount = 0;
+    	boolean moved = false;
+    	while (true) {    
             Observation obs = observe()[0];
-            System.out.println(obs);
-            
+            //System.out.println(obs);
             
             //Schreibt in Map den Typ des Objektes auf das geschaut wird
-            map[obs.position.y][obs.position.x] = obs.classId;
-            
-            printOutMap(mapSize.height, mapSize.width);
+            //map[obs.position.y][obs.position.x] = obs.classId;
+            //printOutMap(mapSize.height, mapSize.width);
             int d = distance(obs.position) - 1;
             if (d == 0) turnRight();
             // Move until the far edge
@@ -39,7 +78,6 @@ public class RoMax_Roamer extends Creature {
             	    }else{
             		turnRight();
             	    }
-            	
             	}else {
             		switch (obsId) {
             		case WALL_CLASS_ID:
@@ -58,15 +96,13 @@ public class RoMax_Roamer extends Creature {
             		}
             		if(!moved) enemySight(obs);
             	}
-            	
                 if (! moveForward()) {
                     // Hit something unexpected!
                     attack();
                     break;
                 }
                 //Schreibt die abgelaufenen Stellen in die Map
-                Observation self = observeSelf();
-                
+                //Observation self = observeSelf();
             }
             
             if (isEnemy(obs)) {
@@ -75,7 +111,6 @@ public class RoMax_Roamer extends Creature {
             }
         }
     }
-    
     
     //Prints out up-to-date map in the console
     private void printOutMap(int height, int width) {
